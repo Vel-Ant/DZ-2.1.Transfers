b11e90bae81e33f0b26691384f4a8d98b28be483
Vel-Ant <vel-ant@yandex.ru>
2022-11-30 14:32:15 +0300

    Added a scanner

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,18 @@
 package ru.netology
 
+import java.util.*
+
 fun main() {
+    val scanner = Scanner(System.`in`)
+    println("Введите сумму перевода в рублях")
 
     val commission= 0.0075
     val commissionMinimum = 35
-    val count = 1_000
+    val count = scanner.nextInt()
     val amount = if (count * commission < 35) (count + commissionMinimum).toInt() else (count + (count * commission)).toInt()
 
     println("Сумма перевода с комиcсией составит: $amount рублей")
 }
+/**
+ * Решил добавить сканер, думаю так будет интереснее
+  */
